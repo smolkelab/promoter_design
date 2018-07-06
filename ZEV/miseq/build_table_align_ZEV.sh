c3caa3a5eb20b596f2acb7df992c416cabed18ac
Ben <Ben>
2018-07-06 21:25:05 +0000

    Fix log file location

--- ZEV/miseq/build_table_align_ZEV.sh
+++ ZEV/miseq/build_table_align_ZEV.sh
@@ -1,1 +1,1 @@
-nohup python ~/facs-seq/mean_extraction/build_table_align.py build_table_align_ZEV.cfg > ~/facs-seq_test/ZEV/build_table_align.log &
+nohup python ~/facs-seq/mean_extraction/build_table_align.py build_table_align_ZEV.cfg > ~/facs-seq_test/ZEV/miseq/build_table_align.log &
