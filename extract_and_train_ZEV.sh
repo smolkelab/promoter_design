8d14de07d0a597d4992aec93502a2c3f12af6e04
bkotopka <benkotopka@gmail.com>
2018-07-05 22:03:26 -0700

    Add logging for master scripts

--- extract_and_train_ZEV.sh
+++ extract_and_train_ZEV.sh
@@ -1,8 +1,8 @@
-./ZEV/miseq/assign_bins_miseq_ZEV.sh
-./ZEV/miseq/build_table_align_ZEV.sh
-./ZEV/nextseq/assign_bins_nextseq_ZEV.sh
-./ZEV/nextseq/build_table_exact_ZEV.sh
-./ZEV/nextseq/fit_means_ZEV.sh
-./ZEV/merge_miseq_nextseq_ZEV.sh
-./ZEV/filter_means_ZEV.sh
-./ZEV/models/train_model_ZEV.sh
+nohup ./ZEV/miseq/assign_bins_miseq_ZEV.sh >> ~/facs-seq_test/ZEV/master.log &
+nohup ./ZEV/miseq/build_table_align_ZEV.sh >> ~/facs-seq_test/ZEV/master.log &
+nohup ./ZEV/nextseq/assign_bins_nextseq_ZEV.sh >> ~/facs-seq_test/ZEV/master.log &
+nohup ./ZEV/nextseq/build_table_exact_ZEV.sh >> ~/facs-seq_test/ZEV/master.log &
+nohup ./ZEV/nextseq/fit_means_ZEV.sh >> ~/facs-seq_test/ZEV/master.log &
+nohup ./ZEV/merge_miseq_nextseq_ZEV.sh >> ~/facs-seq_test/ZEV/master.log &
+nohup ./ZEV/filter_means_ZEV.sh >> ~/facs-seq_test/ZEV/master.log &
+nohup ./ZEV/models/train_model_ZEV.sh >> ~/facs-seq_test/ZEV/master.log &
