6b7a547e01e5ed3566da55a52a4455e0bc529bc5
Ben <Ben>
2018-07-18 06:51:16 +0000

    Create needed dirs

--- seq_design/build_design_cfgs.sh
+++ seq_design/build_design_cfgs.sh
@@ -1,2 +1,7 @@
 mkdir -p designs
+mkdir -p ~/facs-seq_test/seq_designs/preds
+mkdir -p ~/facs-seq_test/seq_designs/rejected
+mkdir -p ~/facs-seq_test/seq_designs/selected
+mkdir -p ~/facs-seq_test/seq_designs/scores
+mkdir -p ~/facs-seq_test/seq_designs/logs
 python build_design_cfgs.py build_design_cfgs_key.csv
