cff84c6b728349a6c33a0acb34a2e49cb124971e
Ben <Ben>
2018-07-23 18:35:13 +0000

    Bugfixes

--- oligo_design/primer_gen.py
+++ oligo_design/primer_gen.py
@@ -8,16 +8,21 @@ import os
 import math
 import numpy as np
 import copy
+import primer3
 
 DNA = ['A','C','G','T']
+DNA_C = {'A':'T', 'C':'G', 'G':'C', 'T':'A'}
 N_PROBS = [0.25, 0.25, 0.25, 0.25]
 MAX_NN_LENGTH = 60 # primer3 default
 PRIMER3_METHOD = 'santalucia' # primer3 default
 MAX_LOOP = 30 # primer3 default
 
+def rc(seq):
+  return(''.join([DNA_C[q] for q in seq])[::-1])
+
 class primer_set(object):
 
-  def __init__(self, cfg)
+  def __init__(self, cfg):
     params = dict(cfg.items('Params'))
     self.toe_anneal = float(params['toe_anneal'])
     self.toe_tolerance = float(params['toe_tolerance'])
@@ -29,40 +34,53 @@ class primer_set(object):
     self.seq_base = params['SEQ']
     self.base_probs = {'N': N_PROBS} # base probabilities for the toehold: include e.g. BsaI sites this way
     for q in self.seq_base:
-      if not q in DNA:
+      if not q in DNA and not q in self.base_probs.keys():
         x = [float(q) for q in params[q].strip().split(':')]
         self.base_probs[q] = x/sum(x)
     self.len_full_add = int(params['len_full_add'])# length of sequence to add to toehold to get something that anneals at self.full_anneal
+    self.add_to_left = params['add_to_left'] == 'True'
     self.mv_conc = float(params['mv_conc']) # 50
     self.dv_conc = float(params['dv_conc']) # 0
     self.dntp_conc = float(params['dntp_conc']) # 0.8
     self.dna_conc = float(params['dna_conc']) # 50
     np.random.seed(int(params['random_seed']))
-    
+
   # Get the annealing temperature of two sequences
   def melting_temp(self, seq):
-  return( primer3.calcTm(seq, mv_conc=self.mv_conc, dv_conc=self.dv_conc, dntp_conc=self.dntp_conc, 
+    return( primer3.calcTm(seq, mv_conc=self.mv_conc, dv_conc=self.dv_conc, dntp_conc=self.dntp_conc,
                       dna_conc=self.dna_conc, max_nn_length=MAX_NN_LENGTH, tm_method=PRIMER3_METHOD, salt_corrections_method=PRIMER3_METHOD) )
-    
+
   # Find whether a concerning heterodimer exists between two sequences
   def get_misprime_tm(self, seq1, seq2, temp):
+    tm_f1 = self.get_misprime_tm_inner(seq1, seq2, temp)
+    tm_r1 = self.get_misprime_tm_inner(seq1, rc(seq2), temp)
+    tm_f2 = self.get_misprime_tm_inner(seq2, seq1, temp)
+    tm_r2 = self.get_misprime_tm_inner(seq2, rc(seq1), temp)
+    return(max(tm_f1, tm_r1, tm_f2, tm_r2))
+
+  def get_misprime_tm_inner(self, seq1, seq2, temp):
     heterodimer = primer3.calcHeterodimer(seq1, seq2,
                     mv_conc=self.mv_conc, dv_conc=self.dv_conc, dntp_conc=self.dntp_conc, dna_conc=self.dna_conc, 
-                    temp_c=temp, max_loop=MAX_LOOP])
+                    temp_c=temp, max_loop=MAX_LOOP)
     if not heterodimer.structure_found:
-	  return 0.
-	return heterodimer.tm
-  
+      return -100.
+    return heterodimer.tm
+
   def true_if_no_misprime(self, seq1, seq2, temp):
     struct_tm = self.get_misprime_tm(seq1, seq2, temp)
-    return struct_tm < temp - self.misprime_tolerance # e.g. if temp = 45, self.misprime_tolerance = 5, OK if t_anneal < 40; will be melted by 45
-    
+    #if(struct_tm >= (temp - self.misprime_tolerance)):
+    #  raise Exception('oops!' + str(seq1) + ' ' + str(seq2) + ' ' + str(struct_tm))
+
+    return struct_tm < (temp - self.misprime_tolerance) # e.g. if temp = 45, self.misprime_tolerance = 5, OK if t_anneal < 40; will be melted by 45
+
   # placeholder, for maybe rejecting e.g. hairpins
   def primer_passed_filter(self, seq):
     return True
-    
+
   # generate a random sequence
-  def get_random_seq(self, parent = self.base_probs):
+  def get_random_seq(self, parent = None):
+    if parent == None: # this is Python's fault
+      parent = self.seq_base
     ans = []
     for b in parent:
       if b in DNA:
@@ -70,33 +88,37 @@ class primer_set(object):
       else:
         ans.append( np.random.choice(DNA, None, self.base_probs[b]) )
     return(''.join(ans))
-    
+
   # get all variants of a random sequence, and find the one with Tm closest to some target
-  def evolve_one_step(self, seq, target):
+  # Compare to a template to see which bases aren't allowed to be changed
+  def evolve_one_step(self, seq, template, target):
+    assert(len(seq) == len(template))
     ans = seq
     err = abs( self.melting_temp(seq) - target )
-    for i in len(seq):
-      for b in DNA:
-        if seq[i] != b:
-          tmp = [q for q in seq]
-          tmp[i] = b 
-          new_err = self.melting_temp(tmp) - target
-          if new_err < err and self.primer_passed_filter(tmp):
-            err = new_err
-            ans = tmp
+    for i in range(len(seq)):
+      if template[i] not in DNA:
+        for b in DNA:
+          if seq[i] != b:
+            tmp = [q for q in seq]
+            tmp[i] = b
+            tmp = ''.join(tmp)
+            new_err = abs(self.melting_temp(tmp) - target)
+            if new_err < err and self.primer_passed_filter(tmp):
+              err = new_err
+              ans = tmp
     return(ans)
-        
+
   def true_if_no_misprimes_to_seqs(self, seq):
-    target_tmp = self.toe_anneal
     if len(self.seqs) == 0:
       return True
-    return(all([self.true_if_no_misprime(seq, q, target_tmp) for q in self.seqs]))
-    
+    return(all([self.true_if_no_misprime(seq, q, self.toe_anneal) for q in self.seqs]))
+
   # evolve a valid new toe first, then extend
   # 'self.evolve_iterations' available to get both tasks done.
   def evolve_new_seq_inner(self):
     seq = self.get_random_seq()
     is_full = False
+    template = self.seq_base
     for i in range(self.evolve_iterations):
       if is_full: # get temperature targets based on what part of the process this is
         target = self.full_anneal
@@ -107,18 +129,23 @@ class primer_set(object):
       tm = self.melting_temp(seq)
       if tm > target_low and tm < target_high:
         if not is_full:
-          is_full = True
-          seq = self.get_random_seq(''.join(['N']*self.len_full_add) + seq)
+          is_full = True; new_pad = ''.join(['N']*self.len_full_add)
+          if self.add_to_left:
+            template = new_pad + seq
+            seq = self.get_random_seq(template)
+          else:
+            template = seq + new_pad
+            seq = self.get_random_seq(template)
         else:
           return(seq)
-      seq = self.evolve_one_step(seq, target)
+      seq = self.evolve_one_step(seq, template, target)
     return(None)
-    
+
   def evolve_new_seq(self):
     seq = self.evolve_new_seq_inner()
     if seq != None:
       if self.true_if_no_misprimes_to_seqs(seq):
-      self.seqs.append(seq)
+        self.seqs.append(seq)
 
 def get_seqs(cfg):
   num_seqs = int(cfg.get('Params','num_seqs'))
@@ -126,23 +153,35 @@ def get_seqs(cfg):
   primers = primer_set(cfg)
   for i in range(num_iters):
     primers.evolve_new_seq()
-	if len(primers.seqs) >= num_seqs:
-	  break
+    if len(primers.seqs) >= num_seqs:
+      break
   if len(primers.seqs) < num_seqs:
     return(None)
   return(primers)
 
 if __name__ == '__main__':
-  cfg = ConfigParser.RawConfigParser(allow_no_value=True)
+  cfg = ConfigParser.RawConfigParser(allow_no_value=True); cfg.optionxform=str
   cfg.read(sys.argv[1])
   primers = get_seqs(cfg)
   for seq in primers.seqs:
     print('Seq: ' + str(seq))
-	toe = seq[-len(cfg.get('Params','SEQ'):]
-	print('Toe: ' + str(toe))
-	print('T_melt (toe): ' + str(primers.melting_temp(toe)))
-	print('T_melt (full): ' + str(primers.melting_temp(seq)))
-	t_ann = float(cfg.get('Params','toe_anneal'))
-	misprimes = [(primers.get_misprime_tm(seq, q, t_ann), q) for q in primers.seqs if q != seq]
-	print('T_misprime (max): ' + str(max([p for (p,q) in misprimes]))
-	print('Worst misprime sequence: ' + str([q for (p,q) in misprimes if p == max([p for (p,q) in misprimes])]))
+    toe = seq[-len(cfg.get('Params','SEQ')):]
+    print('Toe: ' + str(toe))
+    print('T_melt (toe): ' + str(primers.melting_temp(toe)))
+    print('T_melt (full): ' + str(primers.melting_temp(seq)))
+    t_ann = float(cfg.get('Params','toe_anneal'))
+    worst_seq = ''
+    worst_t = -1000.
+    for q in primers.seqs:
+      if q != seq:
+        new_t = primers.get_misprime_tm(seq, q, t_ann)
+        if new_t > worst_t:
+          worst_t = new_t; worst_seq = q
+
+    print('T_misprime (max): ' + str(worst_t))
+    print('Worst misprime sequence: ' + str(worst_seq))
+  print('Sequences generated: ' + str(len(primers.seqs)))
+  if len(sys.argv) > 2:
+    with open(sys.argv[2], 'w') as fo:
+      for seq in primers.seqs:
+        fo.write(seq + '\n')
--- oligo_design/table_to_pools.py
+++ oligo_design/table_to_pools.py
@@ -12,7 +12,7 @@ import os
 import numpy as np
 import pandas as pd
 import ConfigParser
-import copy
+import StringIO
 import oligo_design
 import primer_gen
 
@@ -21,6 +21,16 @@ PAD_LENS = { 'GPD': '25,25', 'ZEV': '58,58' }
 # How long should the pool-specific primer sites be? For GPD, constrained by the lengtb of the promoters vs. the oligos.
 POOL_TOEHOLD_LENS = { 'GPD': '17,17', 'ZEV': '25,25' }
 
+# Copying ConfigParsers properly is a little tricky.
+# Solution cf. https://stackoverflow.com/questions/23416370/manually-building-a-deep-copy-of-a-configparser-in-python-2-7
+def copy_cfg(cfg):
+  cfg_str = StringIO.StringIO()
+  cfg.write(cfg_str)
+  cfg_str.seek(0)
+  cfg_out = ConfigParser.RawConfigParser()
+  cfg_out.readfp(cfg_str)
+  return(cfg_out)
+
 # Given a single "master" config, modify it for one specific oligo pool.
 # Keys to reset:
 # pad_lens = 25,25
@@ -28,8 +38,8 @@ POOL_TOEHOLD_LENS = { 'GPD': '17,17', 'ZEV': '25,25' }
 # rev_pools = GCGAGCCTTCCCAACGT,TCGGCGAGCCCAGAAGT,ATATCATCTGTCCTGAC
 # assembly_id = test
 
-def customize_cfg(cfg, table, keyname, primer_gen, fwd_pool_gen, rev_pool_gen, num_fwd, num_rev):
-  c = copy.copy(cfg)
+def customize_cfg(cfg, table, keyname, fwd_pool_gen, rev_pool_gen, num_fwd, num_rev):
+  c = copy_cfg(cfg)
   lib_id = table['Library'].unique(); assert(len(lib_id) == 1)
   c.set('Params','pad_lens',PAD_LENS[lib_id[0]])
   c.set('Params','assembly_id',c.get('Params','assembly_id') + '_' + str(keyname))
@@ -51,7 +61,7 @@ def start_file_at_line(fn_in, start_line):
       x = fi.next()
     while True:
       yield(fi.next().strip())
-  
+
 if __name__ == '__main__':
   # load data
   cfg = ConfigParser.RawConfigParser(allow_no_value=True)
@@ -69,17 +79,17 @@ if __name__ == '__main__':
   fwd_pool_gen = start_file_at_line(fn_fwd_pool, start_fwd_pool)  # open the file; start at a specified line
   rev_pool_gen = start_file_at_line(fn_rev_pool, start_rev_pool)
   for q in pool_names:
-    cfgs[q] = customize_cfg(cfg, dfs[q], q, fwd_pool_gen, rev_pool_gen, num_fwd = 2, num_rev = 2)
+    cfgs[q] = customize_cfg(copy_cfg(cfg), dfs[q], q, fwd_pool_gen, rev_pool_gen, num_fwd = 2, num_rev = 2)
   finals = []; rejects = []
   for q in pool_names:
     d = dfs[q]; c = cfgs[q]
-    seqs = d['Seq']
+    seqs = d['Seq'].tolist()
     final_oligos, rejected = oligo_design.seqs_to_oligos(seqs, c)
     finals.extend(final_oligos)
     rejects.extend(rejected)
 
   with open(os.path.expanduser(cfg.get('Files','oligo_fn')),'w') as fo:
-    for name, oligo in final_oligos:
+    for name, oligo in finals:
       fo.write(name + '\n')
       fo.write(oligo + '\n')
   with open(os.path.expanduser(cfg.get('Files','rejected_fn')),'w') as fr:
