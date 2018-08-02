18e47a31adaed448d71358861ddc666c2ad61325
bkotopka <benkotopka@gmail.com>
2018-08-02 15:39:10 -0700

    Fixing bugs in shell command

--- seq_design/run_designs.py
+++ seq_design/run_designs.py
@@ -6,13 +6,14 @@ SCRIPTS_DIR = 'designs'
 SCRIPTS_TABLE = 'build_design_cfgs_key.csv'
 
 def main(this_instance, num_instances):
-  scripts = [q for q in os.listdir(SCRIPTS_DIR) if q[-2:] == 'sh']
+  df = pd.read_csv(SCRIPTS_TABLE)
+  os.chdir(SCRIPTS_DIR)
+  scripts = [q for q in os.listdir('.') if q[-2:] == 'sh']
   tags = [int(q.split('_')[0]) for q in scripts]
   tag_to_script = dict(zip(tags, scripts))
-  df = pd.read_csv(SCRIPTS_TABLE)
   for i in df['ID']:
     if i % num_instances == this_instance:
-      os.system('./' + os.path.join(SCRIPTS_DIR, tag_to_script[i]))
+      os.system('./' + tag_to_script[i])
 
 if __name__ == '__main__':
   [this_instance, num_instances] = [int(q) for q in sys.argv[1:]]
