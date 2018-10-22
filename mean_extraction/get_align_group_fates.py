4feece611fee731f09166da98621b7788154fd9e
Ben <Ben>
2018-10-22 00:54:47 +0000

    bugfixes

--- mean_extraction/get_align_group_fates.py
+++ mean_extraction/get_align_group_fates.py
@@ -8,7 +8,7 @@ import os
 import ConfigParser
 
 class fate_summary(object):
-  def __init__(cfg):
+  def __init__(self, config):
     self.fn_cluster_output = os.path.expanduser(config.get('Files_Intermediate', 'cluster_output'))
     self.fn_ambig = os.path.expanduser(config.get('Output', 'file_ambig'))
     self.fn_N = os.path.expanduser(config.get('Output', 'file_N'))
@@ -25,34 +25,34 @@ class fate_summary(object):
     self.lines_to_reads(self.fn_N, 'reads_N')
     self.lines_to_groups(self.fn_aln, 'reads_aln')
     
-    def _new_record(self, id):
-      self.keyset.add(id)
-      self.records[id] = {'reads_aln':0, 'reads_N':0, 'reads_ambig':0}
+  def _new_record(self, idx):
+    self.keyset.add(idx)
+    self.records[idx] = {'reads_aln':0, 'reads_N':0, 'reads_ambig':0}
     
-    def lines_to_reads(self, fn, key_id):
-      with open(fn, 'r') as fi:
-        for l in fi:
-          gp_id = int(l.strip().split(',')[-1])
-          if gp_id not in self.keyset:
-            self._new_record(gp_id)
-          self.records[id][key_id] = self.records[id][key_id] + 1
+  def lines_to_reads(self, fn, key_id):
+    with open(fn, 'r') as fi:
+      for l in fi:
+        gp_id = int(l.strip().split(',')[-1])
+        if gp_id not in self.keyset:
+          self._new_record(gp_id)
+        self.records[gp_id][key_id] = self.records[gp_id][key_id] + 1
     
-    def lines_to_groups(self, fn, key_id): # group ID is field 1, 2 on up are reads (sum these)
-      with open(fn, 'r') as fi:
-        for l in fi:
-          l = l.strip().split(',')[1:]
-          gp_id = int(l[0])
-          num_reads = sum([int(q) for q in l[1:]])
-          if gp_id not in self.keyset:
-            self._new_record(gp_id)
-          self.records[id][key_id] = self.records[id][key_id] + num_reads         
+  def lines_to_groups(self, fn, key_id): # group ID is field 1, 2 on up are reads (sum these)
+    with open(fn, 'r') as fi:
+      for l in fi:
+        l = l.strip().split(',')[1:]
+        gp_id = int(l[0])
+        num_reads = sum([int(q) for q in l[1:]])
+        if gp_id not in self.keyset:
+          self._new_record(gp_id)
+        self.records[gp_id][key_id] = self.records[gp_id][key_id] + num_reads
     
-    def write_to_file(self):
-      with open(self.fn_out, 'w') as fo:
-        fo.write('Group,Ambig,N,Aln\n')
-        for k in self.keyset:
-          rec = self.records[k]
-          fo.write(','.join([str(q) for q in [k, rec['reads_ambig'], rec['reads_N'], rec['reads_aln']]]) + '\n')
+  def write_to_file(self):
+    with open(self.fn_out, 'w') as fo:
+      fo.write('Group,Ambig,N,Aln\n')
+      for k in self.keyset:
+        rec = self.records[k]
+        fo.write(','.join([str(q) for q in [k, rec['reads_ambig'], rec['reads_N'], rec['reads_aln']]]) + '\n')
       
 
 def main_method(cfg):
