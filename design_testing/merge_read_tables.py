6ef9906bc00a6b7efd40b0873a2fae0b36511bde
bkotopka <benkotopka@gmail.com>
2018-11-12 15:47:53 -0800

    Fix bug, using pandas to concatenate

--- design_testing/merge_read_tables.py
+++ design_testing/merge_read_tables.py
@@ -1,10 +1,11 @@
 import os
+import pandas as pd
 
 GPD_READ_TABLE_FN = os.path.expanduser('~/facs-seq_test/design_testing/miseq_GPD/final_merged_reads_GPD.csv')
 ZEV_READ_TABLE_FN = os.path.expanduser('~/facs-seq_test/design_testing/miseq_ZEV/final_merged_reads_ZEV.csv')
 OUT_FN = os.path.expanduser('~/facs-seq_test/design_testing/merged_read_table.csv')
-with open(GPD_READ_TABLE_FN, 'r') as fg, open(ZEV_READ_TABLE_FN, 'r') as fz, open(OUT_FN, 'w') as fo:
-  for l in fg:
-    fo.write(l)
-  for l in fz:
-    fo.write(l)
+
+pd_gpd = pd.read_csv(GPD_READ_TABLE_FN)
+pd_zev = pd.read_csv(ZEV_READ_TABLE_FN)
+pd_out = pd.concat([pd_gpd, pd_zev])
+pd_out.to_csv(OUT_FN)
