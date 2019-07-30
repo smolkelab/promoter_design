cf8630624ecbbd045bc3a8ee21329554308c488c
bkotopka <benkotopka@gmail.com>
2019-07-29 19:44:24 -0700

    Delete extract_and_train_GPD.sh

--- extract_and_train_GPD.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ~/facs-seq_test/GPD
-nohup ./GPD/miseq/assign_bins_miseq_GPD.sh >> ~/facs-seq_test/GPD/master.log &
-nohup ./GPD/miseq/build_table_align_GPD.sh >> ~/facs-seq_test/GPD/master.log &
-nohup ./GPD/nextseq/assign_bins_nextseq_GPD.sh >> ~/facs-seq_test/GPD/master.log &
-nohup ./GPD/nextseq/build_table_exact_GPD.sh >> ~/facs-seq_test/GPD/master.log &
-nohup ./GPD/nextseq/fit_means_GPD.sh >> ~/facs-seq_test/GPD/master.log &
-nohup ./GPD/merge_miseq_nextseq_GPD.sh >> ~/facs-seq_test/GPD/master.log &
-nohup ./GPD/filter_means_GPD.sh >> ~/facs-seq_test/GPD/master.log &
-nohup ./GPD/models/train_model_GPD.sh >> ~/facs-seq_test/GPD/master.log &
