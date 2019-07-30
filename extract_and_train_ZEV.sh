c7aab88f621b3a103960d219ddeae5f88762a44a
bkotopka <benkotopka@gmail.com>
2019-07-29 19:44:33 -0700

    Delete extract_and_train_ZEV.sh

--- extract_and_train_ZEV.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ~/facs-seq_test/ZEV
-nohup ./ZEV/miseq/assign_bins_miseq_ZEV.sh >> ~/facs-seq_test/ZEV/master.log &
-nohup ./ZEV/miseq/build_table_align_ZEV.sh >> ~/facs-seq_test/ZEV/master.log &
-nohup ./ZEV/nextseq/assign_bins_nextseq_ZEV.sh >> ~/facs-seq_test/ZEV/master.log &
-nohup ./ZEV/nextseq/build_table_exact_ZEV.sh >> ~/facs-seq_test/ZEV/master.log &
-nohup ./ZEV/nextseq/fit_means_ZEV.sh >> ~/facs-seq_test/ZEV/master.log &
-nohup ./ZEV/merge_miseq_nextseq_ZEV.sh >> ~/facs-seq_test/ZEV/master.log &
-nohup ./ZEV/filter_means_ZEV.sh >> ~/facs-seq_test/ZEV/master.log &
-nohup ./ZEV/models/train_model_ZEV.sh >> ~/facs-seq_test/ZEV/master.log &
