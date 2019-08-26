9dcd40dfecc1c2f420122dde4fe619082c016731
bkotopka <benkotopka@gmail.com>
2019-08-25 22:55:14 -0700

    Justify choice of tests

--- Figures/RStudio/5.R
+++ Figures/RStudio/5.R
@@ -5,6 +5,7 @@
 setwd('D:/Promoter Design Data/')
 library(data.table)
 library(ggplot2)
+library(reshape2)
 source('Code/FCS file analysis.R')
 
 get.one.median = function(fn) {
@@ -12,22 +13,41 @@ get.one.median = function(fn) {
   rats = log10(x[,10]/x[,8])
   return(median(rats))
 }
+
+melt.for.jitter = function(df, mode) {
+  if (mode == 'Uninduced') {df = data.frame(Name=df$Name, `Design Type`=df$`Design Type`, R1=df$R1_FALSE, R2=df$R2_FALSE, R3=df$R3_FALSE)}
+  if (mode == 'Induced') {df = data.frame(Name=df$Name, `Design Type`=df$`Design Type`, R1=df$R1_TRUE, R2=df$R2_TRUE, R3=df$R3_TRUE)}
+  if (mode == 'AR') { df = data.frame(Name=df$Name, `Design Type`=df$`Design Type`, R1=df$R1_TRUE - df$R1_FALSE, 
+                                      R2=df$R2_TRUE - df$R2_FALSE, R3=df$R3_TRUE - df$R3_FALSE) }
+  if (mode == 'All') {df = data.frame(Name=df$Name, `Design Type`=df$`Design Type`, R1=df$R1_FALSE, R2=df$R2_FALSE, R3=df$R3_FALSE,
+                                      R4=df$R1_TRUE, R5=df$R2_TRUE, R6=df$R3_TRUE)}
+  if (mode == 'All') {df = melt(df, id.vars = c('Name', 'Design.Type'), measure.vars = c('R1', 'R2', 'R3', 'R4', 'R5', 'R6'))}
+  else { df = melt(df, id.vars = c('Name', 'Design.Type'), measure.vars = c('R1', 'R2', 'R3')) }
+  df = data.table(df)
+  setnames(df, 'Design.Type', 'Design Type')
+  df$value = 10^df$value
+  return(df)
+}
+
+
 w = 9.5
 h = 6
 
 dat = fread('Validation/validation_output_fs_means_corrected.csv'); dat$V1 = NULL
+dat$SEM.scale = 1/sqrt(3)
+dat$SEM.scale[dat$Name %in% c('GPD', 'CYC1', 'TEF', 'ADH1', 'PGK1', 'TPI1')] = 1/sqrt(6)
 dat$VYB_Mean_A = apply(cbind(dat$R1_FALSE, dat$R2_FALSE, dat$R3_FALSE), 1, mean)
 dat$VYB_SD_A = apply(cbind(dat$R1_FALSE, dat$R2_FALSE, dat$R3_FALSE), 1, sd)
 dat$VYB_Mean_B = apply(cbind(dat$R1_TRUE, dat$R2_TRUE, dat$R3_TRUE), 1, mean)
 dat$VYB_SD_B = apply(cbind(dat$R1_TRUE, dat$R2_TRUE, dat$R3_TRUE), 1, sd)
 dat$VYB_AR = dat$VYB_Mean_B - dat$VYB_Mean_A
 dat$VYB_AR_SD = sqrt(dat$VYB_SD_A^2 + dat$VYB_SD_B^2)
-dat$bar.min.a = dat$VYB_Mean_A-dat$VYB_SD_A
-dat$bar.max.a = dat$VYB_Mean_A+dat$VYB_SD_A
-dat$bar.min.b = dat$VYB_Mean_B-dat$VYB_SD_B
-dat$bar.max.b = dat$VYB_Mean_B+dat$VYB_SD_B
-dat$bar.min.ar = dat$VYB_AR-dat$VYB_AR_SD
-dat$bar.max.ar = dat$VYB_AR+dat$VYB_AR_SD
+dat$bar.min.a = dat$VYB_Mean_A-(dat$VYB_SD_A*dat$SEM.scale)
+dat$bar.max.a = dat$VYB_Mean_A+(dat$VYB_SD_A*dat$SEM.scale)
+dat$bar.min.b = dat$VYB_Mean_B-(dat$VYB_SD_B*dat$SEM.scale)
+dat$bar.max.b = dat$VYB_Mean_B+(dat$VYB_SD_B*dat$SEM.scale)
+dat$bar.min.ar = dat$VYB_AR-(dat$VYB_AR_SD*dat$SEM.scale)
+dat$bar.max.ar = dat$VYB_AR+(dat$VYB_AR_SD*dat$SEM.scale)
 
 # Did we choose this sequence because we expect it to be unreliable?
 dat$trusted_a = dat$Scale_A == 0
@@ -80,19 +100,20 @@ dev.off()
 # 5B
 # break log scaling
 dat = fread('Validation/validation_output_fs_means_corrected.csv'); dat$V1 = NULL
-
+dat$SEM.scale = 1/sqrt(3)
+dat$SEM.scale[dat$Name %in% c('GPD', 'CYC1', 'TEF', 'ADH1', 'PGK1', 'TPI1')] = 1/sqrt(6)
 dat$VYB_Mean_A = apply(cbind(dat$R1_FALSE, dat$R2_FALSE, dat$R3_FALSE), 1, mean)
 dat$VYB_SD_A = apply(cbind(dat$R1_FALSE, dat$R2_FALSE, dat$R3_FALSE), 1, sd)
 dat$VYB_Mean_B = apply(cbind(dat$R1_TRUE, dat$R2_TRUE, dat$R3_TRUE), 1, mean)
 dat$VYB_SD_B = apply(cbind(dat$R1_TRUE, dat$R2_TRUE, dat$R3_TRUE), 1, sd)
 dat$VYB_AR = dat$VYB_Mean_B - dat$VYB_Mean_A
 dat$VYB_AR_SD = sqrt(dat$VYB_SD_A^2 + dat$VYB_SD_B^2)
-dat$bar.min.a = 10^(dat$VYB_Mean_A-dat$VYB_SD_A)
-dat$bar.max.a = 10^(dat$VYB_Mean_A+dat$VYB_SD_A)
-dat$bar.min.b = 10^(dat$VYB_Mean_B-dat$VYB_SD_B)
-dat$bar.max.b = 10^(dat$VYB_Mean_B+dat$VYB_SD_B)
-dat$bar.min.ar = 10^(dat$VYB_AR-dat$VYB_AR_SD)
-dat$bar.max.ar = 10^(dat$VYB_AR+dat$VYB_AR_SD)
+dat$bar.min.a = 10^(dat$VYB_Mean_A-dat$VYB_SD_A*dat$SEM.scale)
+dat$bar.max.a = 10^(dat$VYB_Mean_A+dat$VYB_SD_A*dat$SEM.scale)
+dat$bar.min.b = 10^(dat$VYB_Mean_B-dat$VYB_SD_B*dat$SEM.scale)
+dat$bar.max.b = 10^(dat$VYB_Mean_B+dat$VYB_SD_B*dat$SEM.scale)
+dat$bar.min.ar = 10^(dat$VYB_AR-dat$VYB_AR_SD*dat$SEM.scale)
+dat$bar.max.ar = 10^(dat$VYB_AR+dat$VYB_AR_SD*dat$SEM.scale)
 
 dat$VYB_Mean_A = 10^(dat$VYB_Mean_A)
 dat$VYB_Mean_B = 10^(dat$VYB_Mean_B)
@@ -115,6 +136,10 @@ dat.gpd$`Design Type`[dat.gpd$`Design Type` == 28] = 'Gradient*'
 dat.gpd$`Design Type` = factor(dat.gpd$`Design Type`)
 
 dat.gpd$Name = factor(dat.gpd$Name, levels = dat.gpd$Name[order(dat.gpd$Experiment.designed, dat.gpd$VYB_Mean_A)])
+#dat.gpd.m = melt.for.jitter(dat.gpd, 'Uninduced')
+dat.gpd.me = melt.for.jitter(dat.gpd[dat.gpd$`Design Type` != 'Control',], 'Uninduced')
+dat.gpd.mc = melt.for.jitter(dat.gpd[dat.gpd$`Design Type` == 'Control',], 'All')
+dat.gpd.m = rbind(dat.gpd.me, dat.gpd.mc)
 
 # for building final sequence-strength table: pGPD
 # Use the full set from S21 instead!
@@ -124,7 +149,8 @@ png(filename = 'D:/Promoter Design Data/Figures/PNGs/5B.png',
     units = 'cm', width = w, height = h, res = 600)
 xstr = c(paste0('pGPD-', 1:15), 'pCYC1', 'pTEF1', 'pGPD')
 p = ggplot(dat.gpd, aes(Name, VYB_Mean_A, color = `Design Type`, ymin = bar.min.a, ymax = bar.max.a)) + 
-  geom_point(size = 1, stroke = 0) + geom_errorbar(size = 0.3) + theme_bw() + 
+  geom_errorbar() + 
+  theme_bw() + 
   theme(plot.title = element_text(hjust = 0.4, size = 8, face='bold')) +
   theme(axis.text = element_text(size=6), axis.title = element_text(size=8, face='bold'),
         legend.text = element_text(size=6), legend.title = element_text(size=8, face='bold'),
@@ -133,7 +159,9 @@ p = ggplot(dat.gpd, aes(Name, VYB_Mean_A, color = `Design Type`, ymin = bar.min.
         axis.ticks.x=element_blank()) +
   scale_x_discrete(labels=xstr) +
   labs(x = '', y='Promoter Activity\npGPD Designs', color = 'Design Type')
-print(p)
+q = p + geom_jitter(data=dat.gpd.m, aes(x=Name, y=value, color = `Design Type`,
+                                        ymin=NULL, ymax=NULL), width = 0.1, height = 0, size = 0.5)
+print(q)
 dev.off()
 
 # 5C
@@ -155,10 +183,11 @@ dat.zev.i$`Design Type` = factor(dat.zev.i$`Design Type`)
 # for building final sequence-strength table: pZEV-I
 dat.zev.i$Seqs.final = sapply(dat.zev.i$Seqs, function(x) substr(x,59,nchar(x)-59))
 #write.csv(dat.zev.i, 'C:/Users/Ben/Dropbox/Lab/Lab Deliverables/Promoter Manuscript/single_seqs_pZEV_I.csv')
-
+dat.zev.i.m = melt.for.jitter(dat.zev.i, 'Induced')
 p = ggplot(dat.zev.i, aes(Name, VYB_Mean_B, color = `Design Type`, 
                       ymin = bar.min.b, ymax = bar.max.b)) + 
-  geom_point(size = 1, stroke = 0) + geom_errorbar() + theme_bw() + 
+  #geom_point(size = 1, stroke = 0) + 
+  geom_errorbar() + theme_bw() + 
   theme(plot.title = element_text(hjust = 0.4, size = 8, face='bold')) +
   theme(axis.text = element_text(size=6), axis.title = element_text(size=8, face='bold'),
         legend.text = element_text(size=6), legend.title = element_text(size=8, face='bold'),
@@ -167,7 +196,9 @@ p = ggplot(dat.zev.i, aes(Name, VYB_Mean_B, color = `Design Type`,
         axis.ticks.x=element_blank()) +
   scale_x_discrete(labels=xstr) +
   labs(x = '', y='Induced Promoter Activity \npZEV-Induced Designs', color = 'Design Type')
-print(p)
+q = p + geom_jitter(data=dat.zev.i.m, aes(x=Name, y=value, color = `Design Type`,
+                                        ymin=NULL, ymax=NULL), width = 0.1, height = 0, size = 0.5)
+print(q)
 dev.off()
 
 # 5D
@@ -184,13 +215,14 @@ dat.zev.ar$`Design Type` = factor(dat.zev.ar$`Design Type`)
 
 # for building final sequence-strength table: pZEV-AR
 dat.zev.ar$Seqs.final = sapply(dat.zev.ar$Seqs, function(x) substr(x,59,nchar(x)-59))
+dat.zev.ar.m = melt.for.jitter(dat.zev.ar, 'AR')
 #write.csv(dat.zev.ar, 'C:/Users/Ben/Dropbox/Lab/Lab Deliverables/Promoter Manuscript/single_seqs_pZEV_AR.csv')
 
 png(filename = 'D:/Promoter Design Data/Figures/PNGs/5D.png',
     units = 'cm', width = w, height = h, res = 600)
 
 p = ggplot(dat.zev.ar, aes(Name, VYB_AR, color = `Design Type`, ymin = bar.min.ar, ymax = bar.max.ar)) + 
-  geom_point(size = 1, stroke = 0) + geom_errorbar() + theme_bw() +
+  geom_errorbar() + theme_bw() +
   theme(plot.title = element_text(hjust = 0.4, size = 8, face='bold')) +
   theme(axis.text = element_text(size=6), axis.title = element_text(size=8, face='bold'),
         legend.text = element_text(size=6), legend.title = element_text(size=8, face='bold'),
@@ -199,7 +231,9 @@ p = ggplot(dat.zev.ar, aes(Name, VYB_AR, color = `Design Type`, ymin = bar.min.a
         axis.ticks.x=element_blank()) +
   scale_x_discrete(labels=xstr) +
   labs(x = '', y='Activation Ratio \npZEV-Activation Ratio Designs', color = 'Design Type')
-print(p)
+q = p + geom_jitter(data=dat.zev.ar.m, aes(x=Name, y=value, color = `Design Type`,
+                                          ymin=NULL, ymax=NULL), width = 0.1, height = 0, size = 0.5)
+print(q)
 dev.off()
 
 # 5E
@@ -235,21 +269,21 @@ dev.off()
 
 ### Information for text ###
 # 95% confidence intervals, with the correct log-normality
-get.cids = function(df) {
-  df$CI_A_low = 10^(log10(df$VYB_Mean_A) - qnorm(0.975)*df$VYB_SD_A/sqrt(3)) # qnorm(0.975) is about 1.96
-  df$CI_A_hi = 10^(log10(df$VYB_Mean_A) + qnorm(0.975)*df$VYB_SD_A/sqrt(3))
-  df$CI_B_low = 10^(log10(df$VYB_Mean_B) - qnorm(0.975)*df$VYB_SD_B/sqrt(3))
-  df$CI_B_hi = 10^(log10(df$VYB_Mean_B) + qnorm(0.975)*df$VYB_SD_B/sqrt(3))
+get.cids = function(df, n = 3) {
+  df$CI_A_low = 10^(log10(df$VYB_Mean_A) - qt(0.975,df=n-1)*df$VYB_SD_A/sqrt(n))
+  df$CI_A_hi = 10^(log10(df$VYB_Mean_A) + qt(0.975,df=n-1)*df$VYB_SD_A/sqrt(n))
+  df$CI_B_low = 10^(log10(df$VYB_Mean_B) - qt(0.975,df=n-1)*df$VYB_SD_B/sqrt(n))
+  df$CI_B_hi = 10^(log10(df$VYB_Mean_B) + qt(0.975,df=n-1)*df$VYB_SD_B/sqrt(n))
   return(df)
 }
 
 # for constant controls, we have 6 measurements: use all of them
-get.cids.ctrl = function(df) {
+get.cids.ctrl = function(df, n = 6) {
   tmp = cbind(df$R1_FALSE, df$R1_TRUE, df$R2_FALSE, df$R2_TRUE, df$R3_FALSE, df$R3_TRUE)
   m = apply(tmp,1,mean)
   s = apply(tmp,1,sd)
-  df$CI_low = 10^(m - qnorm(0.975)*s/sqrt(6)) # qnorm(0.975) is about 1.96
-  df$CI_hi = 10^(m + qnorm(0.975)*s/sqrt(6))
+  df$CI_low = 10^(m - qt(0.975,df=n-1)*s/sqrt(n))
+  df$CI_hi = 10^(m + qt(0.975,df=n-1)*s/sqrt(n))
   return(df)
 }
 
@@ -285,8 +319,6 @@ for(i in 1:nrow(tmp)) {
   print(t.test(tmp[i,], g, 'less')$p.value)
 }
 
-
-
 # Designed pZEV-I
 dat.tmp = dat.zev.i[dat.zev.i$Experiment.designed == 34,]
 dat.tmp = dat.tmp[dat.tmp$VYB_Mean_B %in% range(dat.tmp$VYB_Mean_B),]
@@ -306,8 +338,6 @@ for(i in 1:nrow(z)) { for(j in 1:nrow(tmp)) {
 }}
 colnames(pvals) = dat.tmp$VYB_Mean_B; rownames(pvals) = dat.ctrl$Name
 
-
-
 # CI for Pr3
 dat.ctrl = get.cids(dat.ctrl)
 print(dat.ctrl$Name)
@@ -316,15 +346,14 @@ print(dat.ctrl$CI_B_low)
 print(dat.ctrl$CI_B_hi)
 
 # Designed pZEV-AR
-get.cids.ar = function(df) {
+get.cids.ar = function(df, n = 3) {
   tmp = cbind(df$R1_TRUE - df$R1_FALSE, df$R2_TRUE - df$R2_FALSE, df$R3_TRUE - df$R3_FALSE)
   m = apply(tmp,1,mean)
   s = apply(tmp,1,sd)
-  df$CI_AR_low = 10^(m - qnorm(0.975)*s/sqrt(3)) # qnorm(0.975) is about 1.96
-  df$CI_AR_hi = 10^(m + qnorm(0.975)*s/sqrt(3))
+  df$CI_AR_low = 10^(m - qt(0.975, n-1)*s/sqrt(n)) # qnorm(0.975) is about 1.96
+  df$CI_AR_hi = 10^(m + qnorm(0.975, n-1)*s/sqrt(n))
   return(df)
 }
-
 
 dat.zev.ar = dat[(is.na(dat$Seqs) & dat$Name %in% c('ZEV_Pr3', 'ZEV_Pr4', 'ZEV_Pr8')) | 
                    dat$Experiment.designed == 44,]# | dat$ZEV_AR_offscale_cand | dat$ZEV_I_AR_cand,]
@@ -341,8 +370,6 @@ dat.tmp = get.cids.ar(dat.tmp)
 print(dat.tmp$VYB_AR)
 print(dat.tmp$CI_AR_low)
 print(dat.tmp$CI_AR_hi)
-
-
 
 # AR for controls
 dat.ctrl = dat[dat$Name %in% c('ZEV_Pr3', 'ZEV_Pr4', 'ZEV_Pr8'),]
@@ -365,8 +392,6 @@ for(i in 1:nrow(z)) { for(j in 1:nrow(tmp)) {
 }}
 colnames(pvals) = dat.tmp$VYB_Mean_A; rownames(pvals) = dat.ctrl$Name
 
-
-
 # INDUCED
 dat.tmp = dat.zev.ar[dat.zev.ar$Experiment.designed == 44,]
 dat.ctrl = dat[dat$Name %in% c('ZEV_Pr3', 'ZEV_Pr4'),]
@@ -378,5 +403,23 @@ for(i in 1:nrow(z)) { for(j in 1:nrow(tmp)) {
 }}
 colnames(pvals) = dat.tmp$VYB_Mean_B; rownames(pvals) = dat.ctrl$Name
 
+# lastly, get the CI table that has the raw numbers for Supplementary Table 3
+dat = fread('Validation/validation_output_fs_means_corrected.csv'); dat$V1 = NULL
+dat$VYB_Mean_A = apply(cbind(dat$R1_FALSE, dat$R2_FALSE, dat$R3_FALSE), 1, mean)
+dat$VYB_SD_A = apply(cbind(dat$R1_FALSE, dat$R2_FALSE, dat$R3_FALSE), 1, sd)
+dat$VYB_Mean_B = apply(cbind(dat$R1_TRUE, dat$R2_TRUE, dat$R3_TRUE), 1, mean)
+dat$VYB_SD_B = apply(cbind(dat$R1_TRUE, dat$R2_TRUE, dat$R3_TRUE), 1, sd)
+dat$VYB_AR = dat$VYB_Mean_B - dat$VYB_Mean_A
+dat$VYB_Mean_A = 10^(dat$VYB_Mean_A)
+dat$VYB_Mean_B = 10^(dat$VYB_Mean_B)
+dat$VYB_AR = 10^(dat$VYB_AR)
+# create columns CI_A_low, CI_A_hi, CI_B_low, CI_B_hi
+dat = get.cids(dat)
+# create columns CI_AR_low, CI_AR_hi
+dat = get.cids.ar(dat)
+# special handling for constant controls: take advantage of the extra replicates
+# create columns CI_low, CI_hi
+dat = get.cids.ctrl(dat)
+write.csv(dat, 'D:/Promoter Design Data/Validation/validation_data_with_CIs.csv')
 
 
--- Figures/RStudio/S21.R
+++ Figures/RStudio/S21.R
@@ -2,23 +2,38 @@ library(data.table)
 library(ggplot2)
 setwd('D:/Promoter Design Data')
 
-dat = fread('Validation/validation_output_fs_means_corrected.csv'); dat$V1 = NULL
+melt.for.jitter = function(df, mode) {
+  if (mode == 'Uninduced') {df = data.frame(Name=df$Name, `Design Type`=df$`Design Type`, R1=df$R1_FALSE, R2=df$R2_FALSE, R3=df$R3_FALSE)}
+  if (mode == 'Induced') {df = data.frame(Name=df$Name, `Design Type`=df$`Design Type`, R1=df$R1_TRUE, R2=df$R2_TRUE, R3=df$R3_TRUE)}
+  if (mode == 'AR') { df = data.frame(Name=df$Name, `Design Type`=df$`Design Type`, R1=df$R1_TRUE - df$R1_FALSE, 
+                                      R2=df$R2_TRUE - df$R2_FALSE, R3=df$R3_TRUE - df$R3_FALSE) }
+  if (mode == 'All') {df = data.frame(Name=df$Name, `Design Type`=df$`Design Type`, R1=df$R1_FALSE, R2=df$R2_FALSE, R3=df$R3_FALSE,
+                                      R4=df$R1_TRUE, R5=df$R2_TRUE, R6=df$R3_TRUE)}
+  if (mode == 'All') {df = melt(df, id.vars = c('Name', 'Design.Type'), measure.vars = c('R1', 'R2', 'R3', 'R4', 'R5', 'R6'))}
+  else { df = melt(df, id.vars = c('Name', 'Design.Type'), measure.vars = c('R1', 'R2', 'R3')) }
+  df = data.table(df)
+  setnames(df, 'Design.Type', 'Design Type')
+  df$value = 10^df$value
+  return(df)
+}
 
 w = 16
 h = 7.5
-
+dat = fread('Validation/validation_output_fs_means_corrected.csv'); dat$V1 = NULL
+dat$SEM.scale = 1/sqrt(3)
+dat$SEM.scale[dat$Name %in% c('GPD', 'CYC1', 'TEF', 'ADH1', 'PGK1', 'TPI1')] = 1/sqrt(6)
 dat$VYB_Mean_A = apply(cbind(dat$R1_FALSE, dat$R2_FALSE, dat$R3_FALSE), 1, mean)
 dat$VYB_SD_A = apply(cbind(dat$R1_FALSE, dat$R2_FALSE, dat$R3_FALSE), 1, sd)
 dat$VYB_Mean_B = apply(cbind(dat$R1_TRUE, dat$R2_TRUE, dat$R3_TRUE), 1, mean)
 dat$VYB_SD_B = apply(cbind(dat$R1_TRUE, dat$R2_TRUE, dat$R3_TRUE), 1, sd)
 dat$VYB_AR = dat$VYB_Mean_B - dat$VYB_Mean_A
 dat$VYB_AR_SD = sqrt(dat$VYB_SD_A^2 + dat$VYB_SD_B^2)
-dat$bar.min.a = 10^(dat$VYB_Mean_A-dat$VYB_SD_A)
-dat$bar.max.a = 10^(dat$VYB_Mean_A+dat$VYB_SD_A)
-dat$bar.min.b = 10^(dat$VYB_Mean_B-dat$VYB_SD_B)
-dat$bar.max.b = 10^(dat$VYB_Mean_B+dat$VYB_SD_B)
-dat$bar.min.ar = 10^(dat$VYB_AR-dat$VYB_AR_SD)
-dat$bar.max.ar = 10^(dat$VYB_AR+dat$VYB_AR_SD)
+dat$bar.min.a = 10^(dat$VYB_Mean_A-dat$VYB_SD_A*dat$SEM.scale)
+dat$bar.max.a = 10^(dat$VYB_Mean_A+dat$VYB_SD_A*dat$SEM.scale)
+dat$bar.min.b = 10^(dat$VYB_Mean_B-dat$VYB_SD_B*dat$SEM.scale)
+dat$bar.max.b = 10^(dat$VYB_Mean_B+dat$VYB_SD_B*dat$SEM.scale)
+dat$bar.min.ar = 10^(dat$VYB_AR-dat$VYB_AR_SD*dat$SEM.scale)
+dat$bar.max.ar = 10^(dat$VYB_AR+dat$VYB_AR_SD*dat$SEM.scale)
 
 dat$VYB_Mean_A = 10^(dat$VYB_Mean_A)
 dat$VYB_Mean_B = 10^(dat$VYB_Mean_B)
@@ -51,10 +66,15 @@ dat.gpd$Name = factor(dat.gpd$Name, levels = dat.gpd$Name[order(dat.gpd$Experime
 
 # for building final sequence-strength table: pGPD
 dat.gpd$Seqs.final = sapply(dat.gpd$Seqs, function(x) substr(x,26,nchar(x)-26))
+dat.gpd.me = melt.for.jitter(dat.gpd[dat.gpd$`Design Type` != 'Control',], 'Uninduced')
+dat.gpd.mc = melt.for.jitter(dat.gpd[dat.gpd$`Design Type` == 'Control',], 'All')
+dat.gpd.m = rbind(dat.gpd.me, dat.gpd.mc)
+
 #write.csv(dat.gpd, 'C:/Users/Ben/Dropbox/Lab/Lab Deliverables/Promoter Manuscript/single_seqs_pGPD.csv')
 xstr = c(paste0('pGPD-', 1:25), 'pCYC1', 'pTEF1', 'pGPD')
 p = ggplot(dat.gpd, aes(Name, VYB_Mean_A, color = `Design Type`, ymin = bar.min.a, ymax = bar.max.a)) + 
-  geom_point(size = 1, stroke = 0) + geom_errorbar(size = 0.3) + theme_bw() + 
+  #geom_point(size = 1, stroke = 0) + 
+  geom_errorbar(size = 0.3) + theme_bw() + 
   theme(plot.title = element_text(hjust = 0.4, size = 10, face='bold')) +
   theme(axis.text = element_text(size=6), axis.title = element_text(size=8, face='bold'),
         legend.text = element_text(size=6), legend.title = element_text(size=8, face='bold'),
@@ -63,7 +83,9 @@ p = ggplot(dat.gpd, aes(Name, VYB_Mean_A, color = `Design Type`, ymin = bar.min.
         axis.ticks.x=element_blank()) +
   scale_x_discrete(labels=xstr) +
   labs(x = '', y='Promoter Activity\nAll Tested pGPD Designs', color = 'Design Type')
-print(p)
+q = p + geom_jitter(data=dat.gpd.m, aes(x=Name, y=value, color = `Design Type`,
+                                        ymin=NULL, ymax=NULL), width = 0.1, height = 0, size = 0.5)
+print(q)
 dev.off()
 
 
